ea79c4057710ff8841a4ecfe1553d6a54591377e
Sophia Yao <sy2743@columbia.edu>
2019-04-21 16:45:25 -0400

    bar

--- src/Components/Bar.js
+++ src/Components/Bar.js
@@ -1,1 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const BackBar=styled.div`
+    background-color: grey;
+    vertical-align: bottom;
+    height:1.5vh;
+    width:100%;
+    position:fixed;
+    bottom:0
+`
+const ProgressBar=styled.div`
+    background-color: yellow;
+    vertical-align: bottom;
+    height:1.5vh;
+    width:${props => props.width}vw;
+    position:fixed;
+    bottom:0;
+    $(pro)
+`
+
+const Div=styled.div`
+    position: relative;
+    height:100vh;
+`
+
+
+class Bar extends Component{
+
+    render(){
+        return(
+            <Div>
+            this
+            <BackBar/><ProgressBar width={this.props.width}/>
+            this!
+            </Div>
+        );
+    }
+
+}
+
+export default Bar;
